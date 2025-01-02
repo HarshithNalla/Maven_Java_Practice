dffaee277dadbcb298e7689e11d61d033775bf5c
harshith <harshithnalla007@gmail.com>
2025-01-02 19:50:12 +0530

    Java Maven Example Initial commit

---
+++ src/main/java/com/app/Java/Maven/JavaMavenAnotherExample/App.java
@@ -0,0 +1,10 @@
+package com.app.Java.Maven.JavaMavenAnotherExample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("22bd1a053c Harshith");
+    }
+}
